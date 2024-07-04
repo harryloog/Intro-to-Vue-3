afc4a79aa4f94e04cf3552ff082c2b5dfcb4ee41
harryloog <harryloog@outlook.com>
2024-07-05 00:46:26 +0300

    11

---
+++ components/ReviewList.js
@@ -0,0 +1,23 @@
+app.component('review-list', {
+    props: {
+        reviews: {
+            type: Array,
+            required: true
+        }
+    },
+    template:
+    /*html*/
+    `
+    <div class="review-container">
+        <h3>Reviews:</h3>
+        <ul>
+            <li v-for="(review, index) in reviews" :key="index">
+                {{ review.name }} gave this {{ review.rating }} stars
+                <br/>
+                "{{ review.review }}"
+                {{ review.name }}{{ review.reco==="Yes"?" recommends this product.":" doesn't recommend this product" }}
+            </li>
+        </ul>
+    </div>
+    `
+})
